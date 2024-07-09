38ace26c9c5df6b84cb509abadc5e437a4a74423
Kelvin <koolkt10@gmail.com>
2024-07-09 04:25:51 +0000

    pycodestyle added

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,5 +10,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """concurrent async function"""
 
-    res = await asyncio.gather(*tuple(wait_random(max_delay) for i in range(n)))
+    res = await asyncio.gather(
+            *tuple(wait_random(max_delay) for i in range(n)))
     return sorted(res)
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,5 +10,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> list[float]:
     """concurrent async function"""
 
-    res = await asyncio.gather(*tuple(task_wait_random(max_delay) for i in range(n)))
+    res = await asyncio.gather(
+            *tuple(task_wait_random(max_delay) for i in range(n)))
     return sorted(res)
