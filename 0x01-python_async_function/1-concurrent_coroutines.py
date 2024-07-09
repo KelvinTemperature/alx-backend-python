eb433384a9a172d050f9e232687ac0762896cb20
foo <foo@bar.com>
2024-07-09 23:02:32 +0100

    annotated typing

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,12 +2,13 @@
 """Concurrent Coroutine"""
 
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """concurrent async function"""
 
     res = await asyncio.gather(
