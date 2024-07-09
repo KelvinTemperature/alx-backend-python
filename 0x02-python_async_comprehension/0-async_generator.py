19befaba9c4f078ac361a0568c640511653fe476
foo <foo@bar.com>
2024-07-10 00:02:30 +0100

    asyn comprehension tasks

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Python Async Comprehension"""
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Asyn Generator Function"""
+    
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
+        
